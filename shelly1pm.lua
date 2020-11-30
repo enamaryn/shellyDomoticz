b81ff449f84a2305a3a61f054cbc1f63ae96f8c2
enamaryn <40593656+enamaryn@users.noreply.github.com>
2020-11-30 05:35:07 +0100

    update shelly1pm.lua
    résolution des erreurs, test sur domoticz 2020.2 (build 12697), fonctionne bien.

--- shelly1pm.lua
+++ shelly1pm.lua
@@ -1,24 +1,19 @@
--- Recuperation des informations issu de /status des shelly
--- a la fin du fichier vous trouverez un bout de code à
--- ajouter dans le fichier pour chaque shelly que vous avez.
--- n'oubliez pas de créer vos Dummy avant de modifier ce script.
--- Nous avons besoin des IDX des dummy.
+-- Recuperation de informations poweer issu de /status des shelly 1PM
+-- A utiliser avec un "http poller" et capteur virtuel "utilisation (electrique)"
 --
 -- 28/11/2020 : Version 1 : Redaction initiale Auteur : enamaryn
 
-    json = (loadfile "/opt/domoticz/scripts/lua/JSON.lua")()  -- For Linux    
-    --  API call
-    local config=assert(io.popen('curl "http://192.168.1.24/status"'))
-    local result = config:read('*a')
-    config:close()
-    output = json:decode(result)
+-- On appel JSON pour decoder le texte donner par shelly a cette adresse http://ip.du.she.lly/status sur un reseau local --
+	json = (loadfile "/opt/domoticz/scripts/lua/JSON.lua")()  -- For Linux    
+	--  API call
+	local config=assert(io.popen('curl "http://192.168.1.24/status"')) -- ip de votre shelly
+	local result = config:read('*a')
+	config:close()
+	output = json:decode(result)
 
-    MyUptime=output.meters.power
-    SoftwareCurrent=output.update.old_version
-    SoftwareNew=output.update.new_version
-    SwitchHallStatus=output.relays[0].ison
-    
-    print(MyUptime)
-    print(SoftwareCurrent)
-    print(SoftwareNew)
-    print('SwitchHall is '..SwitchHallStatus)
+-- déclaration des variables avec les bonnes données issu du json --
+	local idx = 34 -- on rentre le bon id du capteur virtuel, capteur Utilisation Electrique ou en anglais Usage electric
+	local energieInstantanee = output.meters[1].power -- on prend l'information dans le tableau JSON
+
+-- on update le capteur virtuel --
+	domoticz_updateDevice(idx,'',energieInstantanee)
